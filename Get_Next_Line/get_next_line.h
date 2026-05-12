d57bbb09fd8b1f3f2dac24feca62d7ec56e33e36
ankalini <ankalini@f4r2s6.paris.42.school>
2026-05-12 12:47:16 +0200

    518

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ankalini <ankalini@learner.42.tech>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/02 19:59:13 by ankalini          #+#    #+#             */
-/*   Updated: 2026/05/04 17:55:07 by ankalini         ###   ########.fr       */
+/*   Created: 2026/05/11 12:07:08 by ankalini          #+#    #+#             */
+/*   Updated: 2026/05/11 12:07:09 by ankalini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	char	*subs;
-	size_t	i;
-
-	if (!s)
-		return (0);
-	if (start >= ft_strlen(s))
-		len = 0;
-	else if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
-	subs = malloc(len + 1);
-	if (!subs)
-		return (0);
-	i = 0;
-	while (i < len)
-	{
-		subs[i] = s[start + i];
-		i++;
-	}
-	subs[i] = '\0';
-	return (subs);
-}
---
+++ Get_Next_Line/get_next_line_utils.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankalini <ankalini@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/11 12:06:52 by ankalini          #+#    #+#             */
+/*   Updated: 2026/05/11 17:43:24 by ankalini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stddef.h>
+#include <string.h>
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return((char *)s);
+			s++;
+	}
+	if ((char)c == '\0')
+		return((char * )s);
+	return (NULL);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		len = 0;
+	else if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	subs = malloc(len + 1);
+	if (!subs)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		subs[i] = s[start + i];
+		i++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	i;
+	int	j;
+	char *result;
+
+	result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	j = 0;
+	i = 0;
+	if (!result)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		result[i + j] = s2[j];
+		j++;
+	}
+	result[i + j] = '\0';
+	return (result);
+}
