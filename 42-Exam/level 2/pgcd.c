7a1a062397ef8754e14df3f64be88f89790489ee
518sbl <alexander17.kalinin@gmail.com>
2026-06-22 11:04:39 +0200

    518

--- 42-Exam/level 2/pgcd.c
+++ 42-Exam/level 2/pgcd.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pgcd(int a, int b)
-{
-    int tmp;
-
-    while (b)
-    {
-        tmp = b;
-        b = a % b;
-        a = tmp;
-    }
-    return (a);
-}
-
 int main(int ac, char **av)
 {
+    int a;
+    int b;
+    int tmp;
+
     if (ac == 3)
-        printf("%d\n", pgcd(atoi(av[1]), atoi(av[2])));
+    {
+        a = atoi(av[1]);
+        b = atoi(av[2]);
+        while (b)
+        {
+            tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        printf("%d\n", a);
+    }
     else
         printf("\n");
     return (0);
--- 42-Exam/level 2/tab_mult.c
+++ 42-Exam/level 2/tab_mult.c
@@ -1,48 +1,54 @@
 #include <unistd.h>
 
-int	ft_atoi(char *str)
+int ft_atoi(const char *str)
 {
-	int n = 0;
+	int	i = 0;
+	int	sign = 1;
+	int res = 0;
 
-	while (*str >= '0' && *str <= '9')
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		n *= 10;
-		n += *str - '0';
-		++str;
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	return (n);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
 }
 
 void	ft_putnbr(int n)
 {
 	if (n >= 10)
-		ft_putnbr(n / 10);
-	char c = (n % 10) + '0';
-	write(1, &c, 1);
-}
-
-void	tab_mult(char *str)
-{
-	int n;
-	int i = 1;
-
-	n = ft_atoi(str);
-	while (i <= 9)
-	{
-		ft_putnbr(i);
-		write(1, " x ", 3);
-		ft_putnbr(n);
-		write(1, " = ", 3);
-		ft_putnbr(i * n);
-		write(1, "\n", 1);
-		i++;
-	}
+		ft_putnbr(n /10);
+	char c = n % 10 + '0';
+	write (1, &c, 1);
 }
 
 int main(int ac, char **av)
 {
+	int	i = 1;
+	int	n;
+
 	if (ac == 2)
-		tab_mult(av[1]);
+	{
+		n = ft_atoi(av[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(n);
+			write(1, " = ", 3);
+			ft_putnbr(i * n);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
 	else
 		write(1, "\n", 1);
 	return (0);
